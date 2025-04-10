3ed013639a5dc4a447929874361adabe8962ee59
Mukize <patrickmukize@gmail.com>
2025-04-10 09:47:03 +0200

    fix(util): measuring dBFS instead of dB SPL.

--- src/util.py
+++ src/util.py
@@ -6,9 +6,15 @@ DEFAULT_SAMPLE_RATE: int = 48000
 
 
 def seconds_to_frames(seconds: float, sample_rate=DEFAULT_SAMPLE_RATE) -> int:
+    """
+    Calculate the amount of frames that lapse over x seconds at y sample rate.
+    """
     return int(DEFAULT_SAMPLE_RATE * seconds)
 
 
 def signal_decibel(data: np.ndarray) -> float:
+    """
+    Calculate the decibel sound pressure level from raw signal data.
+    """
     rms = np.sqrt(np.mean(np.square(np.abs(data))))
-    return 20 * math.log10(rms)
+    return 20 * math.log10(rms / 20e-6)
