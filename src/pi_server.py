24a311488aa1f518fd4cc84d7c73704139e410be
Mukize <patrickmukize@gmail.com>
2025-04-15 10:01:45 +0200

    refactor(pi-server): adjust naming and flow

--- src/pi_server.py
+++ src/pi_server.py
@@ -2,21 +2,25 @@ from gpiozero import DigitalInputDevice
 from dotenv import load_dotenv
 from os import getenv
 import socket
-from util import CHANNEL, udp_socket
-
-load_dotenv()
-sensor = DigitalInputDevice(getenv("CHANNEL", CHANNEL))
+from util import DEFAULT_CHANNEL, udp_socket
 
 
-def conn_handler(addr, sock: socket.socket):
-    print(f"Sending data to {addr}")
-    while True:
-        if sensor.value:
-            sock.sendto("Noise detected.".encode(), addr)
-    print(f"Stopped sending data to {addr}")
+def main():
+    load_dotenv()
+    sensor = DigitalInputDevice(getenv("CHANNEL", DEFAULT_CHANNEL))
+
+    def conn_handler(addr, sock: socket.socket):
+        print(f"Sending data to {addr}")
+        while True:
+            if sensor.value:
+                sock.sendto("Noise detected.".encode(), addr)
+        print(f"Stopped sending data to {addr}")
+
+    try:
+        udp_socket(conn_handler)
+    except KeyboardInterrupt:
+        pass
 
 
-try:
-    sock = udp_socket(conn_handler)
-except KeyboardInterrupt:
-    pass
+if __name__ == "__main__":
+    main()
--- src/util.py
+++ src/util.py
@@ -9,7 +9,7 @@ from typing import Callable
 
 DEFAULT_HOST = "127.0.0.1"
 DEFAULT_PORT = "1234"
-DEFAULT_CHANNEL = 17
+PI_CHANNEL = 17
 DEFAULT_SAMPLE_RATE: int = 48000
 
 
@@ -35,9 +35,11 @@ def udp_socket(connection_handler: Callable[[tuple, socket.socket], None]):
     load_dotenv()
     host = os.getenv("SERVER_HOST", DEFAULT_HOST)
     port = int(os.getenv("SERVER_PORT", DEFAULT_PORT))
+
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.bind((host, port))
         print(f"Listening on {host}:{port}")
+        # get a pool of thread workers
         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as pool:
             while True:
                 _, addr = sock.recvfrom(1024)
